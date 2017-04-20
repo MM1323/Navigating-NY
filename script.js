b42df384b6ca83198eba497e854ba1a56012ff4d
Mia McDuffie <mmcduffie@cristoreyny.org>
2017-04-20 12:44:49 +0000

    started the js for the food page

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 // JavaScript File
-function openCity(cityName,elmnt,color) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(cityName).style.display = "block";
-    elmnt.style.backgroundColor = color;
 
-}
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+
+$(document).ready(function(){
+        $('#search').click(function(){
+            var search_adress = $('#zip_code').val();
+            var search_food = $('#type_food').val();
+            console.log(search_food);
+        })
+    })
 
 
 
 
-// Experiment
+// Food Page Get Results and Append
+
+//$(document).ready(function() {
+//    $('#search').click(function() {
+//        //$("#added_info").empty();
+//        var search_adress = $('#zip_code').val();
+//        var search_food = $('#type_food').val();
+//        var api_url_1 = 'https://api.foursquare.com/v2/venues/search?v=20161016&near='
+//        var api_url_2 = '&query='
+//        var api_url_3 = '&intent=checkin&client_id=LAICFUR5GWDY5MBLEFZCEMXEEYTR0KDQU4MKJVWWU33VSITA&client_secret=RWLSS5MCT3DWVRKKOGOU1ZNBIA4EJGG5TE1NBVHSJWJCPH5Q'
+//        var complete_url = api_url_1 + search_adress + api_url_2 + search_food + api_url_3
+//        alert(search_adress)
+//        //$.getJSON(complete_url, function(response) {
+            //console.log(response)
+//        }
+        
+ //   }
+    
+//}
+    
