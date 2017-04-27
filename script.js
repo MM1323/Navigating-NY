45163baf3877594eb710d8f26c3a70e37bd20696
Mia McDuffie <mmcduffie@cristoreyny.org>
2017-04-27 01:51:53 +0000

    creating basic appended results

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 $(document).ready(function(){
         $('#search').click(function(){
+            $("#appended_results").empty();
             var search_adress = $('#zip_code').val();
             var search_food = $('#type_food').val();
             var api_url_1 = 'https://api.foursquare.com/v2/venues/search?v=20161016&near='
@@ -11,8 +12,18 @@ $(document).ready(function(){
             var complete_url = api_url_1 + search_adress + api_url_2 + search_food + api_url_3;
             $.getJSON(complete_url, function(response) {
                 console.log(response);
-             var restaurant_name = response.response.venues[0].name;
-             $('#appended_results').append('<li>' + 'You can go to ' + restaurant_name + "</li>")
+            for (var i=1; i<= 9; i++){
+                var restaurant_name = response.response.venues[i].name;
+                var restaurant_phone = response.response.venues[i].contact.formattedPhone;
+                var restaurant_adress = response.response.venues[i].location.address;
+               // $('#appended_results').append('<li>' + 'You can go to ' + restaurant_name + "." + " " + 'You can call them at ' + restaurant_phone + "." + " " + 'There address is  ' + restaurant_adress + "." + '</li>');
+               // $('#appended_results').append('<div class="col-xs-3 card" style="background-color:lavender;">' + '<p>' + restaurant_name + '</p>' + '<p>' + restaurant_adress + '</p>' + '<p>' + restaurant_phone + '</p>');
+               $('#appended_results').append('<li>' + 'You can go to ' + restaurant_name + "</li>")
+               $('#appended_results').append('<li>' + 'You can call them at ' + restaurant_phone + "</li>")
+               $('#appended_results').append('<li>' + 'There address is  ' + restaurant_adress + "</li>" + "<br>")
+                
+            }
+             
             })
             
         })
